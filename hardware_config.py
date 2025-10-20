19fc5948e165512e6a81a032a432ad6d7881a115
bsae99465 <bsae99465@gmail.com>
2025-10-20 10:00:43 +0700

    Create hardware_config.py

---
+++ hardware_config.py
@@ -0,0 +1,18 @@
+# hardware_config.py
+
+# --- Bill Acceptor (BTC G7RTN) Configuration ---
+BILL_ACCEPTOR_PIN = 32
+PULSE_PER_UNIT_VALUE = 10 
+
+# --- Coin Hopper Configuration ---
+HOPPER_CONTROL_PIN = 27
+HOPPER_FEEDBACK_PIN = 26
+PAYOUT_PULSE_MS = 50   
+
+# --- TM1637 Display Configuration ---
+TM1637_CLK_PIN = 21  # กำหนดขา CLK ของ TM1637
+TM1637_DIO_PIN = 22  # กำหนดขา DIO ของ TM1637
+
+# --- System & Other ---
+RELAY_ON = 0    
+RELAY_OFF = 1
